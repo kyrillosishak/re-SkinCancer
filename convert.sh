bcf0a37d9e8f76a8f606487693a707b266b68667
Kyrillos Ishak <32110652+kyrillosishak@users.noreply.github.com>
2024-08-20 16:03:26 +0300

    Add files via upload

---
+++ convert.sh
@@ -0,0 +1,20 @@
+#!/bin/zsh
+
+# Find all markdown files in the Markdown directory and store them in an array
+SOURCES=($(find Markdown -type f -name "*.md"))
+
+# Create the notebooks directory if it doesn't exist
+mkdir -p notebooks
+
+# Loop over the array of source files
+for SOURCE in "${SOURCES[@]}"; do
+  # Derive the target notebook filename
+  TARGET="notebooks/$(basename "${SOURCE%.md}.ipynb")"
+
+  # Convert markdown to ipynb using pandoc
+  if pandoc --resource-path=assets/ --embed-resources --standalone --from markdown-yaml_metadata_block --wrap=none "$SOURCE" -o "$TARGET"; then
+    echo "Converted $SOURCE to $TARGET"
+  else
+    echo "Failed to convert $SOURCE"
+  fi
+done
